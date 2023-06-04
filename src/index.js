e00b6a45edd62bc5ad76f3c7591bf5648d1b7d7b
Idan Stark <idanstark42@gmail.com>
2023-06-04 16:47:08 +0300

    exporting all modules

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
 
+export { default as Input } from './components/input'
+export { default as Dialog } from './components/dialog'
+export { default as Popup } from './components/popup'
+
 export { default as Context } from './helpers/context'
+export { default as Cache } from './helpers/cache'
+export { default as useForceUpdate } from './helpers/force-update'
+export { default as useLoading } from './helpers/loading'
+export { default as useRealm } from './helpers/realm'
+
+export { default as RealmModel } from './realm/model'
+export { default as RealmUser } from './realm/user'
 
 export function init (props) {
   ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
